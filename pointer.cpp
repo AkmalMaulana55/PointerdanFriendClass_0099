1b5787d93e8d9a906011f27ed12ffd9cb25d6220
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-05-06 15:18:21 +0700

    menambahkan persegi panjang

---
+++ pointer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: 
+    int panjang, lebar;
+
+public:
+
+    void input()
+    {
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "Masukkan lebar : ";
+        cin >> lebar ;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
+
+    Lingkaran ol;
+
+};
+
+
